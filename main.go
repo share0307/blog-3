57cef7b64d365b0eca89bcc56122c3a7aa1f7d74
Kwin <kwin@promote.cache-dns.local>
2021-03-15 12:40:11 +0800

    增加注释

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
-
 	"github.com/zxysilent/logs"
 )
 
@@ -20,14 +19,27 @@ import (
 // @Host 127.0.0.1:88
 // @BasePath /
 func main() {
+	// 打印日志
 	logs.Info("app initializing")
+	// 初始化配置
 	conf.Init()
+	// 初始化模型
 	model.Init()
+
+	// 创建channel，用于监听系统信号
 	quit := make(chan os.Signal)
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL)
+
 	logs.Info("app running")
+
+	// 启用协程，用于运行项目
 	go router.RunApp()
+
+	// 登录退出信号
 	<-quit
+
 	logs.Info("app quitted")
+
+	// 释放日志
 	logs.Flush()
 }
